5a9bd20a6ba6f1df0192cf9caa652a882b85a124
IML <shin10256@gmail.com>
2020-12-20 19:57:01 +0900

    custom

--- js/script.js
+++ js/script.js
@@ -14,50 +14,6 @@ Load Page
 	});
 
 
-/*-------------------------------------------
-Ajax link page transitions
----------------------------------------------*/
-
-	$("a.ajax-link").live("click", function(){
-		$this = $(this);
-		var link = $this.attr('href');
-		var current_url = $(location).attr('href');	
-		
-		if( link != current_url && link != '#' ) { 
-		$.ajax({
-			url:link,
-			processData:true, 
-			dataType:'html', 
-			success:function(data){
-				document.title = $(data).filter('title').text(); 
-				current_url = link;
-        if (typeof history.pushState != 'undefined') history.pushState(data, 'Page', link);
-        
-          setTimeout(function(){						
-          $('#preloader').delay(50).fadeIn(600);
-          $('html, body').delay(1000).animate({ scrollTop:  0  },1000);						
-					
-					setTimeout(function(){
-							
-            $('#ajax-content').html($(data).filter('#ajax-content').html());
-            $('#ajax-sidebar').html($(data).filter('#ajax-sidebar').html());
-
-						$('body').waitForImages({
-							finished: function() {
-								Website();
-								backLoading();
-								$('.opacity-nav').delay(50).fadeOut(600);
-              },										
-              waitForAll: true
-						});								
-					},1000);
-					},0);
-			}
-		});
-    }
-    return false;
-	});
-
 
 /*-------------------------------------------
 When you click back arrow
@@ -84,7 +40,7 @@ function Website() {
 		backgroundmenu();
 		setTimeout(function(){
 			$(".preloader").fadeOut(500);							
-		},2000);
+		},500);
 		setTimeout(function(){
 			$('header').fadeIn();							
 		},500);
